2c7cc5263f33e2b2eb92d255df9ab550ebbff3a1
kenta-saika <kenta.saika@force-corp.co.jp>
2021-07-02 10:31:03 +0900

    save

--- js/if-for.js
+++ js/if-for.js
@@ -17,9 +17,31 @@
 
 // }
 
-const nameList = ["田中","山田","林","佐藤"];
+// const nameList = ["田中","山田","林","佐藤"];
 
-for (let i = 0; i < nameList.length ; i++) {
-   console.log(`${i+1}.${nameList[i]}`);
+// for (let i = 0; i < nameList.length ; i++) {
+//    console.log(`${i+1}.${nameList[i]}`);
 
+// }
+
+// for (let i = 0; i < 10; i++){
+//    if (i % 2 === 0) continue;
+//    console.log(i);
+// }
+
+// for(let i = 1; i < 1000; i++){
+//    if(i >10) break;
+//    console.log(i);
+// }
+
+function scoreCheck() {
+   let testScore = document.getElementById('score').value
+
+if (testScore >= 80){
+    document.write("合格です！おめでとうございます！");
+} else if (testScore >= 70) {
+    document.write("惜しい！あと少し！");
+} else {
+    document.write("不合格です！もう少し勉強しましょう");
+ }
 }
